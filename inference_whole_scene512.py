fc79a070cd31cd98a56514a8c79dd7f543eab32e
tanh86 <48080084+tanh86@users.noreply.github.com>
2019-03-07 14:25:04 +0000

    Update inference_whole_scene512.py

--- inference_whole_scene512.py
+++ inference_whole_scene512.py
@@ -9,29 +9,9 @@ import glob
 from PIL import Image
 import scipy.ndimage as ndimage
 import cv2
+from utils import sliding_window, convert_to_rgb
 
-def sliding_window(image, stepSize, windowSize):
-	for y in range(0, image.shape[0], stepSize):
-		for x in range(0, image.shape[1], stepSize):
-			yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
 
-def convert_to_rgb(pred):
-        Unlabelled = [0,0,0]
-        Ear = [0, 255, 192]
-        EarRegion = [204, 255, 204]
-        labels = np.array([Unlabelled,EarRegion,Ear])
-        red = pred.copy()
-        green = pred.copy()
-        blue = pred.copy()
-        for label in range(0,3):
-            r[pred==label]=labels[label,0]
-            g[pred==label]=labels[label,1]
-            b[pred==label]=labels[label,2]
-        rgb = np.zeros((pred.shape[0], pred.shape[1], 3))
-        rgb[:,:,0] = (red)
-        rgb[:,:,1] = (green)
-        rgb[:,:,2] = (blue)
-        return rgb
 
 fcn8 =  build_full_fcn8(512)
 
@@ -67,9 +47,9 @@ for i in range(0,len(ImList)):
         pred = np.argmax(pred,axis=-1).reshape((1944, 2592))
         #print(pred.shape)
         pred = convert_to_rgb(pred)
-        print(pred.shape)
-        print(np.unique(pred))
+        #print(pred.shape)
+        #print(np.unique(pred))
         pred = Image.fromarray(pred.astype(np.uint8))
         pred = pred.convert('RGB')
         pred.save(filename[:-5]+"_"+ImList[i])
-        
+                
